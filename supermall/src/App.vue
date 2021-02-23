9d2812dcab0767e9f17c3cbead6167f16a733d02
wapony <wapony@126.com>
2021-02-23 19:38:18 +0800

    添加项目目录

--- supermall/src/App.vue
+++ supermall/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
+<script>
+import MainTabBar from 'components/content/maintabbar/MainTabBar'
+
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
 <style>
+
+@import 'assets/css/base.css';
+
 </style>

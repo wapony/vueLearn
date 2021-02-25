34c8a09d8a612b909da09405512ef00419437049
wapony <wapony@126.com>
2021-02-25 18:06:02 +0800

    添加配置文件

--- supermall/vue.config.js
+++ supermall/vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            // 配置路径的别名, 默认已经有了@别名 表示src路径
-            alias: {
-                'assets': '@/assets',
-                'common': '@/common',
-                'components': '@/components',
-                'network': '@/network',
-                'views': '@/views'
-            }
-        }
+  configureWebpack: {
+    resolve: {
+      // 配置路径的别名, 默认已经有了@别名 表示src路径
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
     }
+  }
 }
